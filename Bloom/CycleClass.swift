b89954e7b3483502ff0e49e7a4b8798735c64a49
Blake Robinson <blakerobinson@Blakes-MacBook-Pro.local>
2016-12-14 14:53:02 -0500

    adopted NSCoding protocol in CycleClass

--- Bloom/CycleClass.swift
+++ Bloom/CycleClass.swift
@@ -8,14 +8,32 @@
 
 import UIKit
 
-class CycleClass: NSObject {
+class CycleClass: NSObject, NSCoding {
     var days: [DayClass]
     
     //MARK: Archiving Paths
     static let DocumentsDirectory = FileManager().urls(for: .documentDirectory, in: .userDomainMask).first!
     static let ArchiveURL = DocumentsDirectory.appendingPathComponent("cycle")
     
-    init(days: [Day]) {
-        self.days = days.map { DayClass(day: $0) }
+    init(days: [DayClass]) {
+        self.days = days
+    }
+    
+    convenience init(days: [Day]) {
+        self.init(days: days.map { DayClass(day: $0) })
+    }
+    
+    //MARK: NSCoding
+    struct PropertyKey {
+        static let days = "days"
+    }
+    
+    required convenience init(coder aDecoder: NSCoder) {
+        let days = aDecoder.decodeObject(forKey: PropertyKey.days) as! [DayClass]
+        self.init(days: days)
+    }
+    
+    func encode(with aCoder: NSCoder) {
+        aCoder.encode(days, forKey: PropertyKey.days)
     }
 }
