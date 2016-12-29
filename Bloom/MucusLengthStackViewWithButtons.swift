895e8356f3ccc52b142250e4047760f50b7650fe
Blake Robinson <blakeprobinson@gmail.com>
2016-12-29 11:55:40 -0500

    created MucusLengthStackViewWithButtons subclass of StackViewWithButtons to allow for type checking in disabling logic in sectionOneStackView

---
+++ Bloom/MucusLengthStackViewWithButtons.swift
@@ -0,0 +1,13 @@
+//
+//  MucusLengthStackViewWithButtons.swift
+//  Bloom
+//
+//  Created by Blake Robinson on 12/29/16.
+//  Copyright © 2016 Blake Robinson. All rights reserved.
+//
+
+import UIKit
+
+class MucusLengthStackViewWithButtons: StackViewWithButtons {
+    
+}
