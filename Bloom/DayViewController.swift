51c209b24244b76ecdf8ecb1d34cc7aa9622cc3a
Blake Robinson <blakeprobinson@gmail.com>
2016-12-30 09:11:36 -0500

    wrote outline of pickerViewData function and used it in the picker view delegate and datasource methods

--- Bloom/DayViewController.swift
+++ Bloom/DayViewController.swift
@@ -116,19 +116,28 @@ class DayViewController: UIViewController, HideLubricationDelegate, UIPickerView
     */
 
 }
-
+//MARK: Picker Delegate and DataSource methods
 extension DayViewController {
+    
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
         return 1
     }
     
     func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        return 1
+        return pickerViewData().count
     }
     
     func pickerView(_ pickerView: UIPickerView,
                              titleForRow row: Int,
                              forComponent component: Int) -> String? {
-        return "hello"
+        return pickerViewData()[row]
+    }
+    
+    func pickerViewData() -> [String] {
+        if day == nil {
+            return ["Today", "Yesterday"]
+        } else {
+            return ["AnotherDay", "Yet another day"]
+        }
     }
 }
