2e1a5af138e1a1a09d2842653de69aebb5c969fb
Blake Robinson <blakeprobinson@gmail.com>
2017-01-09 15:44:12 -0500

    made sender on performSegue function a cycle to easily add it to the destination VC in prepare for segue

--- Bloom/AllCyclesCollectionViewController.swift
+++ Bloom/AllCyclesCollectionViewController.swift
@@ -28,31 +28,26 @@ class AllCyclesCollectionViewController: UICollectionViewController {
 
 
     // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using [segue destinationViewController].
-        // Pass the selected object to the new view controller.
+        let destination = segue.destination as! CycleTableViewController
+        destination.cycle = sender as? Cycle
     }
 
 
     // MARK: UICollectionViewDataSource
 
     override func numberOfSections(in collectionView: UICollectionView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
         return 1
     }
 
 
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of items
         return persistenceManager.getIDs().count
     }
 
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath)
-        
-        // Configure the cell
     
         return cell
     }
@@ -60,7 +55,7 @@ class AllCyclesCollectionViewController: UICollectionViewController {
     // MARK: UICollectionViewDelegate
     
     override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        performSegue(withIdentifier: "AllCyclesToCycle", sender: self)
+        performSegue(withIdentifier: "AllCyclesToCycle", sender: persistenceManager.getAllCyclesSorted()[indexPath.row])
     }
 
     /*
