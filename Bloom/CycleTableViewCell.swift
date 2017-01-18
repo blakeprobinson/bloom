b802b294f3606ef9cff55a4ae44a099896d2d955
Blake Robinson <blakeprobinson@gmail.com>
2017-01-18 09:32:07 -0500

    added category property to CycleTableViewCell

--- Bloom/CycleTableViewCell.swift
+++ Bloom/CycleTableViewCell.swift
@@ -13,7 +13,26 @@ class CycleTableViewCell: UITableViewCell {
     @IBOutlet weak var daySymbol: UIView!
     @IBOutlet weak var dayNumber: UILabel!
     
-    
+    var category: AllCyclesCollectionViewCell.Category? {
+        didSet {
+            var color: UIColor?
+            if let category = category {
+                switch category {
+                case .bleeding:
+                    color = UIColor(red:0.80, green:0.00, blue:0.00, alpha:1.0)
+                    daySymbol.layer.borderWidth = 0
+                case .dry:
+                    color = UIColor(red:0.42, green:0.66, blue:0.31, alpha:1.0)
+                    daySymbol.layer.borderWidth = 0
+                case .mucus:
+                    color = UIColor.white
+                    daySymbol.layer.borderColor = UIColor(red:1.00, green:0.49, blue:0.98, alpha:1.0).cgColor
+                    daySymbol.layer.borderWidth = 2
+                }
+                daySymbol.backgroundColor = color
+            }
+        }
+    }
     
     override func awakeFromNib() {
         super.awakeFromNib()
