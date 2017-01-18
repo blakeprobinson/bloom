3976fb1dd4749f68a63aab2d8c4d67ffe86a48b2
Blake Robinson <blakeprobinson@gmail.com>
2017-01-18 14:18:46 -0500

    added extension to UIBezierPath to draw intercourse heart on cycleTableView and wired to VC

--- Bloom/IntercourseHeart.swift
+++ Bloom/IntercourseHeart.swift
@@ -10,12 +10,50 @@ import UIKit
 
 class IntercourseHeart: UIView {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
     override func draw(_ rect: CGRect) {
-        // Drawing code
+        let path = UIBezierPath()
+        path.getHearts(originalRect: rect, scale: 0.9)
+        UIColor(red:0.80, green:0.00, blue:0.00, alpha:1.0).setFill()
+        UIColor(red:0.80, green:0.00, blue:0.00, alpha:1.0).setStroke()
+        path.fill()
+        path.stroke()
     }
-    */
 
+}
+
+public extension UIBezierPath  {
+    
+    func getHearts(originalRect: CGRect, scale: Double) -> UIBezierPath {
+        
+        //Scaling will take bounds from the originalRect passed
+        let scaledWidth = (originalRect.size.width * CGFloat(scale))
+        let scaledXValue = ((originalRect.size.width) - scaledWidth) / 2
+        let scaledHeight = (originalRect.size.height * CGFloat(scale))
+        let scaledYValue = ((originalRect.size.height) - scaledHeight) / 2
+        
+        let scaledRect = CGRect(x: scaledXValue, y: scaledYValue, width: scaledWidth, height: scaledHeight)
+        self.move(to: CGPoint(x: originalRect.size.width/2, y: (scaledRect.origin.y + scaledRect.size.height)))
+        
+        self.addCurve(to: CGPoint(x: scaledRect.origin.x, y: (scaledRect.origin.y + (scaledRect.size.height/4))),
+                      controlPoint1:CGPoint(x: (scaledRect.origin.x + (scaledRect.size.width/2)), y: (scaledRect.origin.y + (scaledRect.size.height*3/4))) ,
+                      controlPoint2: CGPoint(x: scaledRect.origin.x, y: (scaledRect.origin.y + (scaledRect.size.height/2))))
+        
+        self.addArc(withCenter: CGPoint(x: (scaledRect.origin.x + (scaledRect.size.width/4)), y: (scaledRect.origin.y + (scaledRect.size.height/4))),
+                              radius: (scaledRect.size.width/4),
+                              startAngle: CGFloat(M_PI),
+                              endAngle: 0,
+                              clockwise: true)
+        
+        self.addArc(withCenter: CGPoint(x: (scaledRect.origin.x + (scaledRect.size.width * 3/4)), y: (scaledRect.origin.y + (scaledRect.size.height/4))),
+                              radius: (scaledRect.size.width/4),
+                              startAngle: CGFloat(M_PI),
+                              endAngle: 0,
+                              clockwise: true)
+        
+        self.addCurve(to: CGPoint(x: originalRect.size.width/2, y: (scaledRect.origin.y + scaledRect.size.height)),
+                      controlPoint1: CGPoint(x: (scaledRect.origin.x + scaledRect.size.width), y: (scaledRect.origin.y + (scaledRect.size.height/2))),
+                      controlPoint2: CGPoint(x: (scaledRect.origin.x + (scaledRect.size.width/2)), y: (scaledRect.origin.y + (scaledRect.size.height*3/4))) )
+        self.close()
+        return self
+    }
 }
