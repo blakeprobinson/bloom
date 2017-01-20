765a1d8509d2f9195bc7867e14d379ab896222bd
Blake Robinson <blakeprobinson@gmail.com>
2017-01-20 12:11:56 -0500

    added calendar extension that takes a weekday string and returns a date

---
+++ Bloom/CalendarExtensions.swift
@@ -0,0 +1,38 @@
+//
+//  CalendarExtensions.swift
+//  Bloom
+//
+//  Created by Blake Robinson on 1/20/17.
+//  Copyright © 2017 Blake Robinson. All rights reserved.
+//
+
+import Foundation
+
+extension Calendar {
+    func date(fromWeekday weekday: String) -> Date? {
+        var date = Date()
+        var dateComponents = DateComponents()
+        
+        switch weekday {
+        case "Today":
+            break
+        case "Yesterday":
+            dateComponents.day = -1
+            date = self.date(byAdding: dateComponents, to: date, wrappingComponents: false)!
+        default:
+            let dateFormatter = DateFormatter()
+            guard let indexInWeek = dateFormatter.weekdaySymbols.index(of: weekday) else {
+                return nil
+            }
+            print(indexInWeek)
+            let today = self.component(.weekday, from: date) - 1
+            if indexInWeek < today {
+                dateComponents.day = indexInWeek - today
+            } else {
+                dateComponents.day = -(7 - indexInWeek + today)
+            }
+            date = self.date(byAdding: dateComponents, to: date, wrappingComponents: false)!
+        }
+        return date
+    }
+}
