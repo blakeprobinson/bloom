1210795cb9aa9de891688763dd2779af59b25eaa
Blake Robinson <blakeprobinson@gmail.com>
2017-01-25 10:16:19 -0500

    refactored displayCycle function in ViewController extension to add dummy cells until today and not until last actual cell of a cycle

--- Bloom/ViewControllerExtensions.swift
+++ Bloom/ViewControllerExtensions.swift
@@ -16,21 +16,29 @@ extension UIViewController {
         for (index, day) in cycle.days.enumerated() {
             if day == cycle.days.last {
                 displayDays.append(day)
-                break
+                let tomorrow = calendar.date(byAdding: .day, value: 1, to: Date())!
+                displayDays = addDaysFrom(calendar.startOfDay(for: day.date), to: calendar.startOfDay(for: tomorrow), in: displayDays, for: cycle)
             } else {
                 displayDays.append(day)
-                let daysBetweenDays = calendar.dateComponents([.day], from: calendar.startOfDay(for: day.date), to: calendar.startOfDay(for: cycle.days[index + 1].date)).day!
-                if daysBetweenDays > 0 {
-                    for index in 1..<daysBetweenDays {
-                        var dateComponents = DateComponents()
-                        dateComponents.day = index
-                        let date = calendar.date(byAdding: dateComponents, to: day.date)!
-                        let day = Day(date: date, uuid: cycle.uuid)
-                        displayDays.append(day)
-                    }
-                }
+                displayDays = addDaysFrom(calendar.startOfDay(for: day.date), to: calendar.startOfDay(for: cycle.days[index + 1].date), in: displayDays, for:cycle)
             }
         }
         return Cycle(days: displayDays, uuid: UUID())
+    }
+    
+    private func addDaysFrom(_ date1: Date, to date2: Date, in displayDays: [Day], for cycle: Cycle) -> [Day] {
+        var days = displayDays
+        let calendar = Calendar(identifier: .gregorian)
+        let daysBetweenDays = calendar.dateComponents([.day], from: date1, to: date2).day!
+        if daysBetweenDays > 0 {
+            for index in 1..<daysBetweenDays {
+                var dateComponents = DateComponents()
+                dateComponents.day = index
+                let date = calendar.date(byAdding: dateComponents, to: date1)!
+                let day = Day(date: date, uuid: cycle.uuid)
+                days.append(day)
+            }
+        }
+        return days
     }
 }
