4f2a0ff4a4a18dcdbcfe09ac8f95a9d2234c635e
Blake Robinson <blakeprobinson@gmail.com>
2017-01-30 14:21:46 -0500

    wrote tests testInsertDayWithoutCycle and testInsertDayWithoutCycle

--- BloomTests/PersistenceManagerTests.swift
+++ BloomTests/PersistenceManagerTests.swift
@@ -10,25 +10,58 @@ import XCTest
 
 class PersistenceManagerTests: XCTestCase {
     
+    let persistenceManager = PersistenceManager()
+    
     override func setUp() {
         super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
-        //use file manager to clean documents folder and delete all cycles if need be...
     }
     
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
-    }
-    
-    func testInsertDayWithoutCycle() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        let persistenceManager = PersistenceManager()
-        print(persistenceManager.getAllCyclesSorted())
-        //let day =
-        //XCTAssertNotNil(day.uuid)
+        //use file manager to clean documents folder and delete all cycles if need be...
+        do {
+            let FM = FileManager()
+            let urls = try FM.contentsOfDirectory(at: persistenceManager.saveDirectory, includingPropertiesForKeys: nil, options: [])
+            urls.forEach({
+                do {
+                    try FM.removeItem(at: $0)
+                } catch {
+                    NSLog("error removing file: \(error)")
+                }
+            })
+        } catch {
+            NSLog("error getting list of url's: \(error)")
+        }
         
     }
     
+    func testInsertDayWithoutCycle() {
+        let day = Day(
+            bleeding: Day.Bleeding.init(intensity: "Moderate"),
+            dry: nil,
+            mucus: nil,
+            observation: 1,
+            intercourse: true,
+            lubrication: true,
+            pasty: false,
+            date: Date(),
+            notes: nil,
+            isFirstDayOfCycle: false)
+        
+        let uuid = persistenceManager.saveDay(day: day)
+        let cycle = persistenceManager.getCycle(uuid: uuid)!
+        XCTAssertTrue(cycle.days.contains(day))
+    }
+    
+    func testInsertDayWithCycle() {
+        let cycle = Cycle(days: [], uuid: UUID())
+        let cycleUuid = persistenceManager.saveCycle(cycle: cycle)
+        let day = Day(date: Date(), uuid: cycle.uuid)
+        let dayUuid = persistenceManager.saveDay(day: day)
+        
+        let retrievedCycle = persistenceManager.getCycle(uuid: dayUuid)!
+        XCTAssertTrue(retrievedCycle.days.contains(day))
+    }
 }
