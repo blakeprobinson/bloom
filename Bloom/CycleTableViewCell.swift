c4785e51e1330660e271dd79cb7441269c2d258c
Blake Robinson <blakeprobinson@gmail.com>
2017-02-01 11:32:42 -0500

    removed category logic from CycleTableViewCell because it is now in DaySymbolView

--- Bloom/CycleTableViewCell.swift
+++ Bloom/CycleTableViewCell.swift
@@ -10,11 +10,7 @@ import UIKit
 
 class CycleTableViewCell: UITableViewCell {
 
-    @IBOutlet weak var daySymbol: UIView! {
-        didSet {
-            daySymbol.layer.cornerRadius = daySymbol.bounds.height / 2
-        }
-    }
+    @IBOutlet weak var daySymbol: DaySymbolView!
     
     
     @IBOutlet weak var date: UILabel!
@@ -22,34 +18,6 @@ class CycleTableViewCell: UITableViewCell {
     @IBOutlet weak var dayDescription: UILabel!
     
     @IBOutlet weak var intercourseHeart: IntercourseHeart!
-    
-    
-    var category: Day.Category? {
-        didSet {
-            var color: UIColor?
-            if let category = category {
-                switch category {
-                case .bleeding:
-                    color = UIColor(red:0.80, green:0.00, blue:0.00, alpha:1.0)
-                    daySymbol.layer.borderWidth = 0
-                case .dry:
-                    color = UIColor(red:0.42, green:0.66, blue:0.31, alpha:1.0)
-                    daySymbol.layer.borderWidth = 0
-                case .mucus:
-                    color = UIColor.white
-                    daySymbol.layer.borderColor = UIColor(red:1.00, green:0.49, blue:0.98, alpha:1.0).cgColor
-                    daySymbol.layer.borderWidth = 2
-                }
-                daySymbol.backgroundColor = color
-                dayNumber.textColor = UIColor.black
-            } else {
-                color = UIColor.lightGray
-                daySymbol.layer.borderWidth = 0
-                daySymbol.backgroundColor = color
-                dayNumber.textColor = UIColor.darkGray
-            }
-        }
-    }
     
     override func awakeFromNib() {
         super.awakeFromNib()
