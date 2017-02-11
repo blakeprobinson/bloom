a008b22ccd910e1dff93c82a509f4c3a05113292
Blake Robinson <blakeprobinson@gmail.com>
2017-02-11 15:54:51 -0500

    Add DayViewModel file
    This file was added to the project, but not to the master branch,
    which created build problems.

---
+++ Bloom/DayViewModel.swift
@@ -0,0 +1,23 @@
+//
+//  DayViewModel.swift
+//  Bloom
+//
+//  Created by Blake Robinson on 2/10/17.
+//  Copyright © 2017 Blake Robinson. All rights reserved.
+//
+
+import Foundation
+
+struct DayViewModel {
+    var saveButtonEnabled = false
+    var dryButtonContainerHidden: Bool?
+    var bleedingButtonContainerHidden: Bool?
+    var mucusButtonContainerHidden: Bool?
+    //var
+}
+
+extension DayViewModel {
+    
+}
+
+//create a DayViewModel with a static method on DayViewModel that takes a day and returns a DayViewModel.  Maybe this happens in a didSet method of the model property on the VC.
